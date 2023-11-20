92fe7dcbedffda775c85597b6c50358e8ff5ff31
Newarin <Newarin@gmail.com>
2023-11-20 09:19:03 +0300

    Homework 5

--- module05/src/main/java/ru/sberbank/edu/App.java
+++ module05/src/main/java/ru/sberbank/edu/App.java
@@ -4,10 +4,22 @@ package ru.sberbank.edu;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        CityInfo moscow = new CityInfo("Moscow", new GeoPosition("55(45'4.4784'')", "37(37'6.3228'')"));
+        //53° 12′ 10″ N, 50° 8′ 27″ E
+        CityInfo samara = new CityInfo("Samara", new GeoPosition("53(12' 10'')", "50(8'27'')"));
+
+        TravelService ts = new TravelService();
+
+        ts.add(moscow);
+        ts.add(samara);
+
+        System.out.println(ts.getDistance("Moscow","Samara"));
+        System.out.println(ts.getCitiesNear("Moscow",1000000).get(1).getName());
+
     }
 }
--- module05/src/main/java/ru/sberbank/edu/GeoPosition.java
+++ module05/src/main/java/ru/sberbank/edu/GeoPosition.java
@@ -15,6 +15,14 @@ public class GeoPosition {
      */
     private double longitude;
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
     /**
      * Ctor.
      *
@@ -24,6 +32,51 @@ public class GeoPosition {
      */
     public GeoPosition(String latitudeGradus, String longitudeGradus) {
         // parse and set latitude and longitude in radian
+
+        int degres = 0;
+        int minutes = 0;
+        double seconds = 0;
+
+        String[] splitLatitude = latitudeGradus.split("\\(|\\'{1,2}\\)*");
+        String[] splitLongitude = longitudeGradus.split("\\(|\\'{1,2}\\)*");
+/**
+ Считаем широту
+ */
+        for(int i =0; i <= splitLongitude.length; i++){
+            switch (i) {
+                case  (0):
+                    degres = Integer.parseInt(splitLongitude[i]);
+                    break;
+                case (1):
+                    minutes = Integer.parseInt(splitLongitude[i]);
+                    break;
+                case (2):
+                    seconds = Double.parseDouble(splitLongitude[i]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        longitude = ( degres + (minutes + seconds /60)/60) * Math.PI/180;
+        /**
+        Считаем долготу
+        */
+        for(int i =0; i <= splitLatitude.length; i++){
+            switch (i) {
+                case  (0):
+                    degres = Integer.parseInt(splitLatitude[i]);
+                    break;
+                case (1):
+                    minutes = Integer.parseInt(splitLatitude[i]);
+                    break;
+                case (2):
+                    seconds = Double.parseDouble(splitLongitude[i]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        latitude = ( degres + (minutes + seconds /60)/60) * Math.PI/180;
     }
 
     // getters and toString
