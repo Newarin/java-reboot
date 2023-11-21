f1106b19307e07bbc25fee36fc79e2c1af69ec28
Newarin <Newarin@gmail.com>
2023-11-21 10:13:03 +0300

    Homework 6

--- module06/src/main/java/ru/sberbank/edu/service/CarService.java
+++ module06/src/main/java/ru/sberbank/edu/service/CarService.java
@@ -1,6 +1,10 @@
 package ru.sberbank.edu.service;
 
+import ru.sberbank.edu.model.Car;
+
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Set;
 
 public interface CarService {
 
@@ -9,5 +13,15 @@ public interface CarService {
     void editModel(String id, String model) throws SQLException;
 
     void deleteCar(String id);
+
+    /**
+     *Реализуем переопределённые методы сервиса
+     */
+    Set<Car> createAll(Collection<Car> cars) throws SQLException;
+    Set<Car> findAll() throws SQLException;
+    Boolean deleteAll() throws SQLException;
+    Set<Car> findByModel(String model) throws SQLException;
+
+
 
 }
